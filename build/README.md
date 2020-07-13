a164749e05cc80cc65ed12a0c39a226d77b2e0b1
Davis Benny <davis.benny@intel.com>
2020-07-13 21:01:23 +0530

    Ubuntu Docker Script for Building AIO
      1. The Docker script is able to build the local copy of source code,
         by mounting the local copy into the container and building the mounted local copy.
    
      2. The script is able to build the latest upstream copy,
         by cloning and building the upstream in /tmp folder of the container and
         finally copying the container/target folder into the mounted local copy.
    
    Signed-off-by: Davis Benny <davis.benny@intel.com>

---
+++ build/README.md
@@ -0,0 +1,36 @@
+# About
+
+Docker Script for Building All-in-one-demo . Using this script you can build the local copy of the repository as well as the latest upstream of the repository.
+
+## Prerequisites
+
+- Operating system: **Ubuntu 18.04.**
+
+- Docker engine : **18.06.0**
+
+- Docker-compose : **1.23.2**
+
+
+## Usage
+
+When you want to build a local copy of the repository.
+
+``` sudo docker-compose up --build ```
+
+When you want to build the latest upstream of the repository.
+
+``` sudo use_remote=1 docker-compose up --build ```
+
+You also have the option to change the remote repository address as well as the remote repository branch in build.sh file.
+
+    REPO_NAME=link-to-your-fork
+    REPO_BRANCH=branch-name
+
+## Expected Outcome
+After successful build, the demo package is available at container/target/aio.tar.gz.
+
+## Updating Proxy Info (Optional )
+If you are working behind a proxy network, ensure that both http and https proxy variables are set.
+
+    export http_proxy=http-proxy-host:http-proxy-port
+    export https_proxy=https-proxy-host:https-proxy-port
---
+++ build/build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Copyright 2020 Intel Corporation
+# SPDX-License-Identifier: Apache 2.0
+
+export http_proxy_host=$(echo $http_proxy | awk -F':' {'print $2'} | tr -d '/')
+export http_proxy_port=$(echo $http_proxy | awk -F':' {'print $3'} | tr -d '/')
+
+export https_proxy_host=$(echo $https_proxy | awk -F':' {'print $2'} | tr -d '/')
+export https_proxy_port=$(echo $https_proxy | awk -F':' {'print $3'} | tr -d '/')
+
+export _JAVA_OPTIONS="-Dhttp.proxyHost=$http_proxy_host -Dhttp.proxyPort=$http_proxy_port -Dhttps.proxyHost=$https_proxy_host -Dhttps.proxyPort=$https_proxy_port"
+
+REPO_PRI=https://github.com/secure-device-onboard/pri.git
+REPO_IOT=https://github.com/secure-device-onboard/iot-platform-sdk.git
+REPO_SCT=https://github.com/secure-device-onboard/supply-chain-tools.git
+REPO_AIO=https://github.com/secure-device-onboard/all-in-one-demo.git
+
+# The same branch will be used to build other SDO components( PRI, IOT & SCT).
+REPO_BRANCH=master
+
+cd /home/sdouser/
+git clone $REPO_IOT
+git clone $REPO_SCT
+git clone $REPO_PRI
+
+cd /home/sdouser/iot-platform-sdk && git checkout $REPO_BRANCH
+mvn clean install
+
+cd /home/sdouser/supply-chain-tools && git checkout $REPO_BRANCH
+mvn clean install
+
+cd /home/sdouser/pri && git checkout $REPO_BRANCH
+mvn clean install
+
+if [ "$use_remote" = "1" ]; then
+  echo "Building $REMOTE_URL : $REMOTE_BRANCH"
+  cd /tmp/
+  git clone $REPO_AIO
+  cd /tmp/all-in-one-demo/
+  git checkout $REPO_BRANCH
+  mvn clean install
+  cp -r ./container/target/ /home/sdouser/all-in-one-demo/container/
+else
+  echo "Building local copy"
+  cd /home/sdouser/all-in-one-demo/
+  mvn clean install
+fi
